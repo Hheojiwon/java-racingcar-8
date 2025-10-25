da102551d0454f4c10e025e3d470669157c4c092
Hheojiwon <0618amy@naver.com>
2025-10-26 04:11:30 +0900

    refactor: output 출력 메서드 이름 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,11 +2,11 @@ package racingcar.view;
 
 public class OutputView {
 
-    public void printNameInputPrompt(){
+    public void printCarNameInputMessage(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public void printCountInputPrompt(){
+    public void printAttemptCountInputMessage(){
         System.out.println("시도할 횟수는 몇 회인가요?");
     }
 }
