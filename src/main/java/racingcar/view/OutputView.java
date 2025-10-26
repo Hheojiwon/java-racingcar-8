ec4da98d008c109c35ba1b7720d39d4d8172b2ff
Hheojiwon <0618amy@naver.com>
2025-10-26 23:55:47 +0900

    feat: 자동차 경주 콘솔 출력 기능을 담당하도록 OutputView 기능 추가 - 각 차수별 경주 결과 출력 - 최종 우승자 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,12 +1,27 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
 
-    public void printCarNameInputMessage(){
+    public void printCarNameInputMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public void printAttemptCountInputMessage(){
+    public void printAttemptCountInputMessage() {
         System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+
+    public void printResultHeader() {
+        System.out.println("실행 결과:");
+    }
+
+    public void printCarStatus(String name, int position) {
+        System.out.println(name + " : " + "-".repeat(position));
+    }
+
+    public void printWinnerResult(List<String> winners) {
+        String winnerText = String.join(", ", winners);
+        System.out.println("최종 우승자 : " + winnerText);
     }
 }
