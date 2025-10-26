ed99201a89bf145471157a98511ceea106e5875b
Hheojiwon <0618amy@naver.com>
2025-10-26 22:54:56 +0900

    feat: 자동차 이름 검증을 위한 Validator 구현

---
+++ src/main/java/racingcar/validator/NameValidator.java
@@ -0,0 +1,22 @@
+package racingcar.validator;
+
+import racingcar.exception.CarException;
+import racingcar.exception.ErrorMessage;
+
+public class NameValidator {
+
+    private final static int NAME_MAX_LENGTH = 5;
+
+    public static void validate(String name){
+        if (name == null){
+            throw new CarException(ErrorMessage.EMPTY_NAME);
+        }
+        if (!checkNameLength(name)){
+            throw new CarException(ErrorMessage.INVALID_NAME_LENGTH);
+        }
+    }
+
+    private static boolean checkNameLength(String name){
+        return name.length() <= NAME_MAX_LENGTH;
+    }
+}
